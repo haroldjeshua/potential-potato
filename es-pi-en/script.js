4fc9c45700ccd34e38ea7b334723ce9e55167a01
Harold Jeshua Varde <harvardii15@gmail.com>
2022-09-30 17:07:23 +0800

    testing out push notif properties

--- es-pi-en/script.js
+++ es-pi-en/script.js
@@ -1,5 +1,18 @@
-const button = document.querySelector('button')
+const button = document.querySelector("button")
 
-button.addEventListener('click', () => {
-    alert('sup')
+button.addEventListener("click", () => {
+	Notification.requestPermission().then(perm => {
+		if (perm === 'granted') {
+			const notif = new Notification('Example Notif', {
+				body: `This is a body text ${Math.random()}`,
+				data: { hello: "world"},
+				icon: "harv-logo.png",
+				// tag: 'Welcome Message'
+			})
+
+			notif.addEventListener('click', e => {
+				console.log(e)
+			})
+		}
+	})
 })
