8347feb74d94d695ca2a48d7d242fd4726d42510
Harold Jeshua Varde <harvardii15@gmail.com>
2022-10-11 23:13:20 +0800

    fixing uncaught for saving list on localstorage

--- ta-da/script.js
+++ ta-da/script.js
@@ -2,16 +2,8 @@ const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 
-let lists = [
-    {
-        id: 1,
-        name: 'name'
-    },
-    {
-        id:2,
-        name: 'todo'
-    }
-]
+const LOCAL_STORAGE_LIST_KEY = 'tasks.lists'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
 
 newListForm.addEventListener('submit', (e) => {
     e.prevetDefault()
@@ -21,11 +13,20 @@ newListForm.addEventListener('submit', (e) => {
     const list = createList(listName)
     newListInput.value = null
     lists.push(list)
-    render()
+    saveAndRender()
 })
 
 function createList(name) {
     return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
 }
 
 function render() {
