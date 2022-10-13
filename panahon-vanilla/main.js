174c7bb4ce59a1f20f3456f6ea244bda48d6027a
Harold Jeshua Varde <harvardii15@gmail.com>
2022-10-13 22:40:03 +0800

    remove apikey

--- panahon-vanilla/main.js
+++ panahon-vanilla/main.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey: '82c16e3433f4ecc1c3426e1332b11ac4',
+    // apiKey: '',
     fetchWeather: function(city) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weather.apiKey}`)
         .then((res) => {
