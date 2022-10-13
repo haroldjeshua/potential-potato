0dc363cc598ce9c85f986124c75e35be614f6189
Harold Jeshua Varde <harvardii15@gmail.com>
2022-10-13 10:28:05 +0800

    selection and saving of list item + deleting list

--- ta-da/script.js
+++ ta-da/script.js
@@ -1,49 +1,83 @@
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+const listDisplayContainer = document.querySelector('[data-list-display-container]')
+const listTitle = listDisplayContainer.querySelector('[data-list-title]')
+const listCount = listDisplayContainer.querySelector('[data-list-count]')
+const tasks = document.querySelector('[data-tasks]')
 
 const LOCAL_STORAGE_LIST_KEY = 'tasks.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'tasks.selectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', (e) => {
+	if (e.target.tagName.toLowerCase() === 'li') {
+		selectedListId = e.target.dataset.listId
+		saveAndRender()
+	}
+})
+
+deleteListButton.addEventListener('click', () => {
+	lists = lists.filter(list => list.id !== selectedListId)
+	selectedListId = null
+	saveAndRender()
+})
 
 newListForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const listName = newListInput.value
+	e.preventDefault()
+	const listName = newListInput.value
 
-    if (listName == null || listName === '') return
-    const list = createList(listName)
-    newListInput.value = null
-    lists.push(list)
-    saveAndRender()
+	if (listName == null || listName === '') return
+	const list = createList(listName)
+	newListInput.value = null
+	lists.push(list)
+	saveAndRender()
 })
 
 function createList(name) {
-    return { id: Date.now().toString(), name: name, tasks: [] }
+	return { id: Date.now().toString(), name: name, tasks: [] }
 }
 
 function saveAndRender() {
-    save()
-    render()
+	save()
+	render()
 }
 
 function save() {
-    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+	localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+	localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
 function render() {
-    clearElement(listsContainer)
-    lists.forEach(list => {
-        const listElement = document.createElement('li')
-        listElement.dataset.listId = list.id
-        listElement.classList.add('list-name')
-        listElement.innerText = list.name
-        listsContainer.appendChild(listElement)
-    });
+	clearElement(listsContainer)
+	renderLists()
+
+	if (selectedListId == null) {
+		listDisplayContainer.style.display = 'none'
+	} else {
+		listDisplayContainer.style.display = ''
+	}
+}
+
+function renderLists() {
+	lists.forEach(list => {
+		const listElement = document.createElement('li')
+		listElement.dataset.listId = list.id
+		listElement.classList.add('list-name')
+		listElement.innerText = list.name
+		if (list.id === selectedListId) {
+			listElement.classList.add('active-list')
+		 }
+		listsContainer.appendChild(listElement)
+	});
 }
 
 function clearElement(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild)
-    }
+	while (element.firstChild) {
+		element.removeChild(element.firstChild)
+	}
 }
 
 render()
