68a76a225c405154d7fb5a2c6e8ee4d9b8c2ebc4
Harold Jeshua Varde <harvardii15@gmail.com>
2022-10-13 18:37:40 +0800

    rendering data to markup

--- panahon-vanilla/main.js
+++ panahon-vanilla/main.js
@@ -1,8 +1,22 @@
 let weather = {
     apiKey: '82c16e3433f4ecc1c3426e1332b11ac4',
-    fetchWeather: (city) => {
+    fetchWeather: function(city) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weather.apiKey}`)
         .then((res) => res.json())
-        .then((data) => console.log(data))
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function(data) {
+        const { name } = data
+        const { icon, description } = data.weather[0]
+        const { temp, humidity } = data.main
+        const { speed } = data.wind
+        console.log(name, icon,description, temp, humidity, speed)
+
+        document.querySelector('.city').innerText = name
+        document.querySelector('.icon').src =  `https://openweathermap.org/img/wn/${icon}.png`
+        document.querySelector('.description').innerText = description
+        document.querySelector('.temp').innerText = temp + "°C"
+        document.querySelector('.humidity').innerText = humidity + "%"
+        document.querySelector('.wind').innerText = speed + " km/h"
     }
 }
