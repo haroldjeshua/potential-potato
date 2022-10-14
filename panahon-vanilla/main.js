e0493f810291bbf9bb85dc8b897f5c0c52196fc4
Harold Jeshua Varde <harvardii15@gmail.com>
2022-10-14 09:53:01 +0800

    Revert "added dynamic bg based on location + fetch error handling"
    This reverts commit 745bba0a20f2cfa34f741f46470de222dd4b22f0.

--- panahon-vanilla/main.js
+++ panahon-vanilla/main.js
@@ -2,13 +2,7 @@ let weather = {
     apiKey: '82c16e3433f4ecc1c3426e1332b11ac4',
     fetchWeather: function(city) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weather.apiKey}`)
-        .then((res) => {
-            if (!res.ok) {
-                alert('No weather found.')
-                throw new Error('No weather found.')
-            }
-            return res.json()
-        })
+        .then((res) => res.json())
         .then((data) => this.displayWeather(data))
     },
     displayWeather: function(data) {
@@ -26,7 +20,6 @@ let weather = {
         document.querySelector('.humidity').innerText = humidity + "%"
         document.querySelector('.wind').innerText = speed + " km/h"
         document.querySelector('.weather').classList.remove('loading')
-        document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`
     },
     search: function() {
         this.fetchWeather(document.querySelector('.search-bar').value)
