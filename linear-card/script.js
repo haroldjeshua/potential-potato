2c86650b60ff4a48d1184ecb6cf16034629122e2
Harold Jeshua Varde <harvardii15@gmail.com>
2022-10-24 23:24:24 +0800

    testing mousemove script without markup canvas

---
+++ linear-card/script.js
@@ -0,0 +1,23 @@
+const el = document.querySelector('.card')
+const wrap = document.querySelector('.card__wrapper')
+
+let w = el.clientWidth
+let h = el.clientHeight
+let b = el.getBoundingClientRect()
+
+el.addEventListener('mousemove', (e) => {
+    let X = (e.clientX - b.left) / w
+    let Y = (e.clientY - b.top) / h
+
+    let rX = -(X - 0.5) * 26
+    let rY = -(Y - 0.5) * 26
+
+    let bgX = 40 + 20 * X
+    let bgY = 40 + 20 * Y
+
+    console.log(X, Y)
+
+    document.documentElement.style.setProperty('--bg-y', bgY + '%')
+    document.documentElement.style.setProperty('--r-x', rX+ 'deg')
+    document.documentElement.style.setProperty('--r-y', rY+ 'deg')
+})
