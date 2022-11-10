e22ca7bed1cc317bc08b1ac80af63989bae3dee2
Harold Jeshua Varde <harvardii15@gmail.com>
2022-11-10 17:03:08 +0800

    add script for theme switching

--- theme-selector/main.js
+++ theme-selector/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+document.addEventListener("click", (e) => {
+  const themeOption = e.target.closest(".theme-option");
+  if (!themeOption) return;
+
+  const themeOptions = document.querySelectorAll(".theme-option");
+
+  themeOptions.forEach((themeOption) =>
+    themeOption.classList.remove("is-selected")
+  );
+
+  const theme = themeOption.dataset.color;
+
+  let root = document.documentElement;
+  root.style.setProperty("--color-primary", theme);
+});
