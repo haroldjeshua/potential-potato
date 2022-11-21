51fbcc3b8dd2309c29b05b87b04a0660241a5f8a
Harold Jeshua Varde <harvardii15@gmail.com>
2022-11-21 22:23:40 +0800

    review pac

--- pack/index.js
+++ pack/index.js
@@ -1,6 +1,6 @@
 const _ = require("lodash");
 
-const numbers = [33, 12312, 3545, 4233234234];
+const numbers = [33, 12312, 3545, 4233, 234234, 1234];
 
 _.each(numbers, function (number, i) {
   console.log(number);
