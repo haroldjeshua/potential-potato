8b957245494777d08429e547f68789ec9cfb1131
Harold Varde <34917956+haroldjeshua@users.noreply.github.com>
2022-12-12 22:22:20 +0800

    added styles + resp grid and colors generate on click

--- grid-staggered/app.js
+++ grid-staggered/app.js
@@ -1,13 +1,37 @@
 "use strict";
 
 const wrapper = document.querySelector(".tiles");
+console.log(wrapper);
 
-let columns = Math.floor(document.body.clientWidth / 50);
-let rows = Math.floor(document.body.clientHeight / 50);
+let columns = 0;
+let rows = 0;
+
+const colors = [
+  "hsl(83, 60%, 69%)",
+  "hsl(138, 60%, 69%)",
+  "hsl(1, 60%, 69%)",
+  "hsl(261, 60%, 69%)",
+  "hsl(318, 60%, 69%)",
+  "hsl(192, 60%, 69%)",
+];
+
+let count = -1;
+
+const handleOnClick = (index) => {
+  //   count += 1;
+  count = count + 1;
+
+  anime({
+    targets: ".tile",
+    backgroundColor: colors[count % (colors.length - 1)],
+  });
+};
 
 const createTile = (index) => {
   const tile = document.createElement("div");
   tile.classList.add("tile");
+
+  tile.onclick = (e) => handleOnClick(index);
   return tile;
 };
 
@@ -23,7 +47,12 @@ const createGrid = () => {
   columns = Math.floor(document.body.clientWidth / 50);
   rows = Math.floor(document.body.clientHeight / 50);
 
+  wrapper.style.setProperty("--columns", columns);
+  wrapper.style.setProperty("--rows", rows);
+
   createTiles(columns * rows);
 };
 
-window.onresize = () => createGrid;
+createGrid();
+
+window.onresize = () => createGrid();
