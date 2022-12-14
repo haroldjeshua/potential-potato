09628f2f0550f7eb78f3fc71fe5804c990269d71
Harold Varde <34917956+haroldjeshua@users.noreply.github.com>
2022-12-14 22:49:11 +0800

    fix styles + adjust logic on neighbor border fx

--- linear-hover/script.js
+++ linear-hover/script.js
@@ -1,27 +1,25 @@
-const handleOnMouseMove = (e) => {
-  const { currentTarget: target } = e;
+// const handleOnMouseMove = (e) => {
+//   const { currentTarget: target } = e;
 
-  const rect = target.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+//   const rect = target.getBoundingClientRect();
+//   const x = e.clientX - rect.left;
+//   const y = e.clientY - rect.top;
 
-  target.style.setProperty("--mouse-x", `${x}px`);
-  target.style.setProperty("--mouse-y", `${y}px`);
+//   target.style.setProperty("--mouse-x", `${x}px`);
+//   target.style.setProperty("--mouse-y", `${y}px`);
+// };
+
+// for (const card of document.querySelectorAll(".card")) {
+//   card.onmousemove = (e) => handleOnMouseMove(e);
+// }
+
+document.querySelector(".cards").onmousemove = (e) => {
+  for (const card of document.querySelectorAll(".card")) {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
 };
-
-for (const card of document.querySelectorAll(".card")) {
-  card.onmousemove = (e) => handleOnMouseMove(e);
-}
-
-document.querySelector(
-  (".cards".onmousemove = (e) => {
-    for (const card of document.querySelectorAll(".card")) {
-      const rect = target.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    }
-  })
-);
