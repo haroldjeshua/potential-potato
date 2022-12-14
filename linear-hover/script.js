b5e3ac621546fac53d07b202ec4d8ad8d2c4b834
Harold Varde <34917956+haroldjeshua@users.noreply.github.com>
2022-12-14 22:15:21 +0800

    added script for card hover fx

---
+++ linear-hover/script.js
@@ -0,0 +1,14 @@
+const handleOnMouseMove = (e) => {
+  const { currentTarget: target } = e;
+
+  const rect = target.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  target.style.setProperty("--mouse-x", `${x}px`);
+  target.style.setProperty("--mouse-y", `${y}px`);
+};
+
+for (const card of document.querySelectorAll(".card")) {
+  card.onmousemove = (e) => handleOnMouseMove(e);
+}
